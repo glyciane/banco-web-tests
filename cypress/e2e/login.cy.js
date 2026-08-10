9f299bcd2bf51ce5e0600794d38fe915f4c96dd6
Glyciane Santos <glyciane1@gmail.com>
2026-08-10 20:54:51 -0300

    Adicionei dados provenientes do fixture

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,8 +8,10 @@ describe('template spec', () => {
   it('Login com dados válidos deve permitir entrada no sistema', () => {
 
     //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
     //cy.screenshot('apos-preencher-dados-validos')
     cy.get('#login-section > .btn').click()
     //cy.screenshot('apos-clicar-no-botao-entrar')
@@ -21,8 +23,10 @@ describe('template spec', () => {
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
 
     //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('654321')
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.invalida.usuario)
+      cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
     cy.contains('button', 'Entrar').click()
 
     //Assert
